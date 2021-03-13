87e2d0d9ec3a1e3aa912aa03b4a54965b7366d26
Floria Xu <xjq0202@outlook.com>
2021-03-13 18:29:40 +0800

    update day5

--- Day5/main.c
+++ Day5/main.c
@@ -8,11 +8,12 @@ int fun5_4(int lim, int aa[100]);
 
 int main()
 {
+   
     //Day5-1
     int a[100];
     int n = 0;
     int i = 0;
-    printf("Please input numbers:\n");
+    printf("(Day5-1)Please input numbers:\n");
     scanf("%d", &n);
     printf("Please input arrys:\n");
     for (i = 0; i < n; ++i)
@@ -25,13 +26,13 @@ int main()
         printf("%d\t", a[i]);
     }
 
-
+    printf("\n");
 
 
     //Day5-2
     int a0[100], x;
     int n0, i0;
-    printf("Please input numbers:\n");
+    printf("(Day5-2)Please input numbers:\n");
     scanf("%d", &n0);
     printf("Please input a:\n");
     for (i0 = 0; i0 < n; ++i0)
@@ -49,39 +50,41 @@ int main()
     }
 
 
-
+    printf("\n");
 
     //Day5-3
     int i1 = 0;
     int n1 = 0;
     int m[100];
     fun5_3(m, &n1);
-    printf("%d\n", n1);
+    printf("(Day5-3)now n is \n%d\n", n1);
     for (i1 = 0; i1 < n1; ++i1)
     {
         printf("%d", m[i1]);
     }
     return 0;
 
-
+    printf("\n");
 
 
     //Day5-4
     int lim, aa[100];
+    printf("(Day5-4)Please input lim:\n");
     scanf("%d", &lim);
     printf("%d\n", fun5_4(lim, aa));
 
 
-
+    printf("\n");
 
     //Day5-5
     int a2[3][3];
     int i2, j, s = 0;
+    printf("(Day5-5)Please input a:\n");
     for (i2 = 0; i2 < 3; ++i2)
     {
         for (j = 0; j < 3; ++j)
         {
-            scanf("%d", &a[i2]);
+            scanf("%d,", &a[i2]);
         }
     }
     for (i2 = 0; i2 < 3; ++i2)
@@ -91,10 +94,12 @@ int main()
     printf("%d\n", s);
 
 
+    printf("\n");
 
 
     //Day5-6
     int a3[3][3], i3, j3;
+    printf("(Day5-6)Please input a:\n");
     for (i3 = 0; i3 < 9; ++i3)
     {
         scanf("%d", &a3[0][0] + i3);
