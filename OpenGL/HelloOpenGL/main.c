85db62c398f74902ebb3700bdb3f3f44a802b0b7
FloriaXu <xjq0202@outlook.com>
2021-08-06 10:53:54 +0800

    Fix linking errors on Mac M1

--- OpenGL/HelloOpenGL/main.c
+++ OpenGL/HelloOpenGL/main.c
@@ -9,8 +9,41 @@
  * 
  */
 #include <stdio.h>
+#include <GLUT/glut.h>
 
-int main(void)
+void initWindow();
+void display();
+void reshape(int width, int height);
+
+int main(int argc, char* argv[])
 {
-    printf("hello OpenGL\n");
+    glutInit(&argc, argv);
+    initWindow();
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void initWindow()
+{
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(600, 400);
+    glutCreateWindow("Hello OpenGL");
+}
+
+void display()
+{
+    glClearColor(1.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
