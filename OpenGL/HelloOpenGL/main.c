413d091af120ee3d8a0c403f32a19a1cd49e407b
FloriaXu <xjq0202@outlook.com>
2021-08-06 10:52:49 +0800

    Init OpenGL HelloWord

---
+++ OpenGL/HelloOpenGL/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Floria Xu
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello OpenGL\n");
+}
