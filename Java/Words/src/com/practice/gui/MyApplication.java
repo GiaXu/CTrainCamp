0c72bc545881ce12a0f4d593b185ad137f28882d
Peter Xu <px.darkside@gmail.com>
2022-03-16 00:34:05 +0800

    add table view

--- Java/Words/src/com/practice/gui/MyApplication.java
+++ Java/Words/src/com/practice/gui/MyApplication.java
@@ -3,13 +3,20 @@ package com.practice.gui;
 import com.practice.controller.DBController;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
+
+import java.util.*;
 
 public class MyApplication extends Application
 {
     private Stage mLoginWindow = null;
     private Stage mSignupWindow = null;
     private Stage mMainWindow = null;
+
+    private ArrayList<DBController.WordItem> mWordItemList = new ArrayList<>();
+    private ObservableList<DBController.WordItem> mDataSource = FXCollections.observableArrayList(mWordItemList);
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -43,7 +50,7 @@ public class MyApplication extends Application
         } // while
 
         if (WindowStates.State.STATE_WORKING == WindowStates.state) {
-            mMainWindow = new InputWindowBuilder().create(primaryStage);
+            mMainWindow = new InputWindowBuilder().create(primaryStage, mDataSource);
             mMainWindow.show();
         } else {
             DBController.release();
